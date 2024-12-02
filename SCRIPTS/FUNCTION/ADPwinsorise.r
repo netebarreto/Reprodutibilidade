2d6116de90df840f99ef080e5b917ada7a1e8d1c
Nete Barreto <naurinete.barreto@gmail.com>
2024-12-02 14:47:31 -0300

    Atualização Funçao Wisorization #11

--- SCRIPTS/FUNCTION/ADPwinsorise.r
+++ SCRIPTS/FUNCTION/ADPwinsorise.r
@@ -22,8 +22,8 @@ ADPwinsorise <- function(iData,iMeta) {
                       X2$resumo<-cbind(iName=b,
                                       Classe=a,
                                       T.NA=n.na,
-                                      LINF=L_inf,
-                                      LSUP=L_sup,
+                                      LINF=round(L_inf,2),
+                                      LSUP=round(L_sup,2),
                                       T.outlier = length(which(X>=L_sup | X<=L_inf)))
                       X2$iData_winsor <- X1
                       X2 = as.list(X2)                  
@@ -36,39 +36,39 @@ ADPwinsorise <- function(iData,iMeta) {
     
     ##### Aplicando para as colunas padrão 
 
-      ncsc =  which(iMeta$Score == 0)   
-      lcolz <- which(colnames(iData) %in% iMeta$iCode[ncsc])
+      ncsc =  which(iMeta$Classe == "Numérico")   
+      lcolz <- which(colnames(iData) %in% iMeta$Code[ncsc])
       list_winsor <- lapply(seq_along(iData[, lcolz]), 
                       function(i) {
-                      yy(iData[, lcolz][[i]], a = "Padrão", b = colnames(iData[, lcolz])[i])})
+                      yy(iData[, lcolz][[i]], a = "Numérico", b = colnames(iData[, lcolz])[i])})
       iData_out[,lcolz] <- do.call(cbind,lapply(list_winsor, function(x) x$iData_winsor))
       resumo_out<-rbind(resumo_out,do.call(rbind,lapply(list_winsor, function(x) x$resumo)))
 
     # Aplicando para as colunas do tipo "cluster" Urbano e Rural
     
-      ncsc =  which(iMeta$Score == -1)   
-      lcolz <- which(colnames(iData) %in% iMeta$iCode[ncsc])
+      ncsc =  which(iMeta$Classe == "Cluster")   
+      lcolz <- which(colnames(iData) %in% iMeta$Code[ncsc])
       nrurb <- which(iData$Cluster == 1) # Cluster Urbano
       nrrur <- which(iData$Cluster == 2) # Cluster Rural
       list_winsor <- lapply(seq_along(iData[nrurb, lcolz]), 
                       function(i) {
-                      yy(iData[nrurb, lcolz][[i]], a = "Cluster Urb", b = colnames(iData[nrurb, lcolz])[i])})
+                      yy(iData[nrurb, lcolz][[i]], a = "Grupo 1", b = colnames(iData[nrurb, lcolz])[i])})
       resumo_out<-rbind(resumo_out,do.call(rbind,lapply(list_winsor, function(x) x$resumo)))
       iData_out[nrurb,lcolz] <- do.call(cbind,lapply(list_winsor, function(x) x$iData_winsor))
 
       list_winsor <- lapply(seq_along(iData[nrrur, lcolz]), 
                       function(i) {
-                      yy(iData[nrrur, lcolz][[i]], a = "Cluster Rural", b = colnames(iData[nrrur, lcolz])[i])})
+                      yy(iData[nrrur, lcolz][[i]], a = "Grupo 2", b = colnames(iData[nrrur, lcolz])[i])})
       resumo_out<-rbind(resumo_out,do.call(rbind,lapply(list_winsor, function(x) x$resumo)))
 
       iData_out[nrrur,lcolz] <- do.call(cbind,lapply(list_winsor, function(x) x$iData_winsor))
     
     # atualizando data.frame para as colunas do tipo "Score"
     
-      ncsc =  which(iMeta_adapta$Score == 1)   
-      lcolz <- which(colnames(iData) %in% iMeta$iCode[ncsc])
+      ncsc =  which(iMeta_adapta$Classe == "Score")   
+      lcolz <- which(colnames(iData) %in% iMeta$Code[ncsc])
       iData_out[,lcolz] <- iData[,lcolz]
-      resumo_out <- rbind(resumo_out,cbind(iMeta$iCode[ncsc],"Score",NA,NA,NA,NA))
+      resumo_out <- rbind(resumo_out,cbind(iMeta$Code[ncsc],"Score",NA,NA,NA,NA))
       result=list(Resumo = as.data.frame(resumo_out),
                   Winsorise = iData_out )
       return(result)
