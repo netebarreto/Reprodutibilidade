ccc4c159fdefd8a2430078133b366d94e575fb85
Nete Barreto <naurinete.barreto@gmail.com>
2025-06-12 10:59:55 -0300

    inclusão do histograma #27

--- SCRIPTS/FUNCTION/F05_ADPGraficos.r
+++ SCRIPTS/FUNCTION/F05_ADPGraficos.r
@@ -46,9 +46,8 @@ gerar_diagrama_setor <- function(data, setor_e, output_file = "diagrama.png", wi
   
   svg <- export_svg(diagram) %>% charToRaw()
   rsvg_png(svg, file = output_file, width = width, height = height)
-  
-  message("Diagrama salvo em: ", output_file)
-}
+  invisible(NULL) 
+  }
 
 
 
@@ -136,4 +135,123 @@ mapa_iBruto = ggplot2::ggplot(data = SHP_0) +
 
   ggplot2::ggsave(filename = nome_arquivo, plot = mapa_iBruto, width = largura, height = altura, dpi = dpi)
 }
+
+mapnorma_result <- function(iNome,DADOS,REFERENCE,
+                Titulo = "Titulo",
+                shp_mun=mun_shp,
+                shp_uf=uf_shp,
+                nome_arquivo = "map_N7.png", 
+                largura = 20, altura = 24, dpi = 25,fsize=16)
+{
+
+
+nome_col = iNome
+
+SHP_0 <- base::merge(shp_mun,cbind(REFERENCE,DADOS) , by.x = "CD_MUN", by.y = "GEOCOD")
+
+SHP_0$classe <- cut(
+  SHP_0[[nome_col]],
+  breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1),
+  include.lowest = TRUE,
+  right = TRUE,
+  labels = c("0–0.2", "0.2–0.4", "0.4–0.6", "0.6–0.8", "0.8–1")
+)
+
+cores <- c("#02c650","#a9de00","#ffcd00","#ff8300","#f40000")
+
+mapa_iBruto <- ggplot2::ggplot(data = SHP_0) +
+  ggplot2::geom_sf(ggplot2::aes(fill = classe), color = NA) +
+  ggplot2::scale_fill_manual(
+    values = cores,
+    drop = FALSE,
+    guide = ggplot2::guide_legend(
+      title.position = "bottom",
+      title.hjust = 0.5,
+      barwidth = 1,
+      barheight = 50
+    )) +
+  ggplot2::geom_sf(data = shp_uf,fill = NA, colour = "black", linewidth=2)+
+  ggplot2::ggtitle(Titulo) +
+  ggplot2::theme_minimal() +  # Usando theme_minimal para um fundo limpo
+  ggplot2::theme(
+    panel.grid = ggplot2::element_blank(),  # Remove as linhas de grade
+    legend.title = ggplot2::element_text(size = fsize),  # Aumenta o tamanho do título da legenda
+    legend.text = ggplot2::element_text(size = fsize),  # Aumenta o tamanho do texto da legenda
+    plot.title = ggplot2::element_text(size = 1.5*fsize),
+    axis.text.x = ggplot2::element_text(size = fsize),  # Tamanho do texto do eixo x
+    axis.text.y = ggplot2::element_text(size = fsize),          
+    panel.border = ggplot2::element_rect(color = "black", fill = NA, 
+                          linewidth = 2)
+  )+
+  ggplot2::coord_sf(xlim = c(-74.1, -34.5), ylim = c(-35, 5), expand = FALSE)
+  # Em caso de erro, cria gráfico com mensagem
+  
+  ggplot2::ggsave(filename = nome_arquivo, plot = mapa_iBruto, width = largura, height = altura, dpi = dpi)
+}
+
+
+
+
+grafico_final <- function(df1 = df1,df = df, 
+nome_arquivo = "grafico_combinado.png", 
+largura = 10, 
+altura = 5, 
+dpi = 25,
+nvalores=nvalores,
+fsize=16) { 
+
+# Crie o gráfico
+scatplot  <- ggplot(df1, aes(x = Normalizado, y = N_Normalizado)) +
+            geom_point(color="blue") +
+            labs(title = paste("Gráfico de Dispersão -",nvalores), x = "Dados Normalizados", y = "Dados Brutos")+
+            theme_minimal() +
+            theme(axis.title.x = element_text(size = fsize,vjust=-0.75),
+              axis.text.x = element_text(size = 0.85*fsize),
+              axis.title.y = element_text(size = fsize),  # Aumentar o tamanho da letra do eixo Y
+              axis.text.y = element_text(size = 0.85*fsize),
+              plot.title = element_text(size = fsize, face = "bold"),
+              panel.border = element_rect(color = "black", fill = NA, 
+                          linewidth = 2),
+            plot.margin = margin(t = 20, r = 20, b = 50, l = 20))  
+  # Criar o histograma com a linha de distribuição normal
+
+# Tenta gerar o histograma normalmente
+histograma <- tryCatch({
+  hist_base  <- hist(df[,1], plot = FALSE)
+  
+  ggplot(df, aes(x = !!rlang::sym(nvalores))) +
+    geom_histogram(aes(y = after_stat(density)),  breaks = hist_base$breaks, fill = "lightblue", color = "blue") +
+    stat_function(fun = dnorm, 
+                  args = list(mean = mean(df[[rlang::sym(nvalores)]]), sd = sd(df[[rlang::sym(nvalores)]])), 
+                  color = "red", linewidth = 1.5) +
+    theme_minimal() +
+    labs(x = nvalores, y = "Densidade", title = paste("Histograma dados Normalizados -",nvalores)) +
+    theme(
+      axis.title.x = element_text(size = fsize),  
+      axis.text.x = element_text(size = 0.85*fsize),  
+      axis.title.y = element_text(size = fsize),  
+      axis.text.y = element_text(size = 0.85*fsize),
+      plot.title = element_text(size = fsize, face = "bold"),
+      panel.border = element_rect(color = "black", fill = NA, linewidth = 2),
+      plot.margin = margin(t = 20, r = 20, b = 50, l = 20)
+    )
+}, error = function(e) {
+  # Em caso de erro, cria gráfico com mensagem
+  ggplot() +
+    annotate("text", x = 0.5, y = 0.5, label = paste("Erro ao gerar gráfico:\n", e$message),
+             size = 6, hjust = 0.5, vjust = 0.5, color = "red") +
+    theme_void() +
+    theme(
+      plot.margin = margin(t = 20, r = 20, b = 50, l = 20),
+      plot.title = element_text(size = fsize, face = "bold", hjust = 0.5)
+    ) +
+    labs(title = paste("Histograma dados Normalizados -", nvalores))
+})
+  graf_combinado <- arrangeGrob(histograma, scatplot, ncol = 2, widths = c(2,2))
+  
+  # Salvar o gráfico combinado em um arquivo PNG
+  ggsave(filename = nome_arquivo, plot = graf_combinado,width = largura, height = altura, dpi = dpi)
+}
+
+
 
