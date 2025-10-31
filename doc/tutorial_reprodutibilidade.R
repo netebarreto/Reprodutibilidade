16192940d73fc4c753fb1a64f5f0582c3d9d642d
Nete Barreto <naurinete.barreto@gmail.com>
2025-10-31 07:46:09 -0300

    Add files via upload

--- doc/tutorial_reprodutibilidade.R
+++ doc/tutorial_reprodutibilidade.R
@@ -5,108 +5,128 @@
 #  require(reprodutibilidade)
 
 ## ----eval = FALSE-------------------------------------------------------------
+#  
 #  library("reprodutibilidade")
+#  
 #  inxlsx <- openxlsx::loadWorkbook(file = "DATASET/Base_inicial_SA_Acesso.xlsx")
-#    name_meta = "Metadados"
-#    name_data = "Dados_RA_Acesso"
+#  
 
 ## ----eval = FALSE-------------------------------------------------------------
-#    metadados <- openxlsx::read.xlsx(inxlsx, sheet = name_meta)
-#    dataset  <- openxlsx::read.xlsx(inxlsx, sheet = name_data)
+#  
+#    metadados <- openxlsx::read.xlsx(inxlsx, sheet = "metadados")
+#    dataset  <- openxlsx::read.xlsx(inxlsx, sheet = "dados_SA_Acesso")
 #  
 #    # Recortando apenas os dados do Nivel 7
 #    metadadosN7 = subset(metadados,metadados$Nivel==7)
 #  
 #    # Selecionando apenas as referencias
-#    data_ref = dataset[,c(1:4)]
+#    data_ref = dataset[,c(1:3)]
 #  
 #    # Selecionando todos os dados
-#    datasetN7 = round(dateset[,-c(1:4)],2)
+#    datasetN7 = round(dateset[,-c(1:3)],2)
 #  
 #    #Atribuindo nome as colunas
-#    colnames(datasetN7) <- colnames(dataset[,-c(1:4)])
+#    colnames(datasetN7) <- colnames(dataset[,-c(1:3)])
 
-## ----echo=TRUE, eval=TRUE, results='markup'-----------------------------------
+## ----echo=TRUE, eval=TRUE, results='markup',fig.width=6.8, fig.height=4, dpi=100----
+library("reprodutibilidade")
+
+# Carregando a base de dados de exemplo
+data("datasetN7", package = "reprodutibilidade")
+
+idx_MMPD = datasetN7$MMPD
+nome = "MMPD"
+
+res1 = criar_resumo(idx_MMPD,"Numerico" , "MMPD")
+
+print_summary(res1)
+
+criar_grafico(idx_MMPD, plot=TRUE, nome_arquivo = "grafico_combinado.png", 
+              largura = 10, altura = 5, dpi = 25, 
+              nvalores="MMPD",fsize=6)
+
+
+## ----eval = TRUE--------------------------------------------------------------
 
 library("reprodutibilidade")
 # Carregando a base de dados de exemplo
-data("iData_N7", package = "reprodutibilidade")
 
-idx_MMPD = iData_N7$MMPD
+# Metadados
+data("metadadosN7", package = "reprodutibilidade")
+
+# Dataset 
+data("datasetN7", package = "reprodutibilidade")
+
+# Dataset referencia
+data("data_ref", package = "reprodutibilidade")
+
+resumo <- ADPresumo(datasetN7, 
+                    metadadosN7$CLASSE,  
+                    colnames(datasetN7))
+
+print(resumo$resumo_total)                    
+
+## ----eval = TRUE--------------------------------------------------------------
+library("reprodutibilidade")
+
+# Carregando a base de dados de exemplo
+data("metadadosN7", package = "reprodutibilidade")
+data("datasetN7", package = "reprodutibilidade")
+
+idx_MMPD = datasetN7$MMPD
 nome = "MMPD"
 
-res1 = criar_resumo(idx_MMPD,"Numerico" , cluster = NULL, "MMPD")
+res1 = winsorize_info(idx_MMPD,"Numerico" , "MMPD")
 
-t(as.data.frame(res1))
+print_summary(res1)
 
 
-## ----eval = FALSE-------------------------------------------------------------
-#  
-#  library("reprodutibilidade")
-#  # Carregando a base de dados de exemplo
-#  data("iMeta_N7", package = "reprodutibilidade")
-#  data("iData_N7", package = "reprodutibilidade")
-#  
-#  # Selecionando apenas as referencias
-#  data_ref = iData_N7[,c(1:4)]
-#  
-#  resumo <- ADPresumo(iData_N7,
-#                      iMeta_N7$Classe,
-#                      data_ref[,4],
-#                      colnames(iData_N7))
+## ----eval = TRUE--------------------------------------------------------------
 
-## ----eval = FALSE-------------------------------------------------------------
-#  
-#  library("reprodutibilidade")
-#  # Carregando a base de dados de exemplo
-#  data("iMeta_N7", package = "reprodutibilidade")
-#  data("iData_N7", package = "reprodutibilidade")
-#  
-#  
-#  winsorize_data(dataset=iData_N7,
-#                 metadados=iMeta_N7,
-#                 cluster_ref=data_ref[,4])
-#  
-#  data_winsor <- winsorize_apply(dataset=iData_N7,
-#                                metadados=iMeta_N7,
-#                                cluster_ref=data_ref[,4])
-#  
+winsorize_data(dataset = datasetN7, 
+               metadados = metadadosN7)
 
-## ----eval = FALSE-------------------------------------------------------------
-#  
-#  library("reprodutibilidade")
-#  
-#  # Carregando a base de dados de exemplo
-#  data("iMeta_N7", package = "reprodutibilidade")
-#  data("iData_N7", package = "reprodutibilidade")
-#  
-#  dados <- iMeta_N7[,7]
-#  
-#  # Aplicando Box-Cox com pacote forecast
-#  bxcx.fcs <- boxcox_transform(dados, metodo = "forecast")
-#  
-#  # Aplicando Box-Cox com o COINr
-#  bxcx.coin <- boxcox_transform(dados,metodo = "COINr")
-#  
-#  # Aplicando Yeo-Johnson
-#  bxcx.yjn <- boxcox_transform(dados, metodo = "yeojohnson")
-#  
-#  par(mfrow=c(1,3)) ; par(mar=c(5,5,6,1))
-#  
-#   plot(c(1:length(dados)),bxcx.fcs,
-#        main = "Transformacao BoxCox \n via pacote Forecast",
-#        cex.axis=1.5,las=1,ylab="Transformada BoxCox",
-#        xlab="Reference", cex.lab=1.8,cex.main=2)
-#  
-#   plot(c(1:length(dados)),bxcx.coin,
-#        main = "Transformacao BoxCox \n via pacote COINr",
-#        cex.axis=1.5,las=1,ylab="Transformada BoxCox",
-#        xlab="Reference",cex.lab=1.8,cex.main=2)
-#  
-#   plot(c(1:length(dados)),bxcx.yjn,
-#        main = "Transformacao BoxCox \n via pacote bestNormalize::Yeojohnson",
-#        cex.axis=1.5,las=1,ylab="Transformada BoxCox",
-#        xlab="Reference",cex.lab=1.8,cex.main=2)
+## ----eval = TRUE, fig.width=6.8, fig.height=4, dpi=100------------------------
+
+data_winsor <- winsorize_apply(dataset=datasetN7,
+                              metadados=metadadosN7)
+
+
+## ----eval = TRUE,fig.width=6.8, fig.height=4, dpi=100-------------------------
+
+library("reprodutibilidade")
+
+# Carregando a base de dados de exemplo
+data("metadadosN7", package = "reprodutibilidade")
+data("datasetN7", package = "reprodutibilidade")
+
+dados <- datasetN7[,19]
+
+# Aplicando Box-Cox com pacote forecast
+bxcx.fcs <- boxcox_transform(dados, metodo = "forecast")
+ 
+# Aplicando Box-Cox com o COINr
+bxcx.coin <- boxcox_transform(dados,metodo = "COINr")
+ 
+# Aplicando Yeo-Johnson
+bxcx.yjn <- boxcox_transform(dados, metodo = "yeojohnson")
+
+par(mfrow=c(1,3)) ; par(mar=c(5,4,6,1))
+ 
+ plot(sort(dados),sort(bxcx.fcs),
+      main = "Transformacao BoxCox \n via pacote Forecast",
+      cex.axis=1,las=1,ylab="Transformada BoxCox",
+      xlab="Reference", cex.lab=0.8,cex.main=1) 
+ 
+ plot(sort(dados),sort(bxcx.coin),
+      main = "Transformacao BoxCox \n via pacote COINr",
+      cex.axis=1,las=1,ylab="Transformada BoxCox",
+      xlab="Reference",cex.lab=0.8,cex.main=1) 
+ 
+ plot(sort(dados),sort(bxcx.yjn),
+      main = "Transformacao BoxCox \n via pacote bestNormalize::Yeojohnson",
+      cex.axis=1,las=1,ylab="Transformada BoxCox",
+      xlab="Reference",cex.lab=0.8,cex.main=1)
 
 ## ----eval = FALSE-------------------------------------------------------------
 #  data_bxcx <- ADPBoxCox(dadoswin=data_winsor$dataset,
